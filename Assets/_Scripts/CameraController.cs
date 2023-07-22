41d0848bde8a1f3607bff5b1b18eacca772d8f85
Matthew MacDonald <mattman2006.mm@gmail.com>
2023-07-22 14:10:18 -0400

    Updated camera and movement controls

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -9,8 +9,16 @@ public class CameraController : MonoBehaviour
 
     //CLASS VARIABLES
     public new Camera camera;
-    public DynamicJoystick joystick;
-    private float speed = 2.0f;
+    public FixedJoystick moveJoystick;
+    public FixedJoystick lookJoystick;
+    public float sensitivity = 0.25f; 
+    public float speed = 10.0f;
+
+    private float horizontalMultiplier;
+    private float verticleMultiplier; 
+    private float xRotation;
+    private float yRotation;
+    
 
 
     // Start is called before the first frame update
@@ -24,40 +32,81 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        //MOVE: the camera left to right, up and down, forward and back
-
-        //move up or forward w/shift key
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || joystick.Vertical > 0)
+        move();
+        look();
+    }
+    
+    private void look()
+    {
+        if(lookJoystick.Horizontal != 0 || lookJoystick.Vertical != 0)
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            {
-                gameObject.transform.position += Vector3.forward * speed * Time.deltaTime;
-            } else
-            gameObject.transform.position += Vector3.up * speed * Time.deltaTime;
+        xRotation = lookJoystick.Vertical * -1;
+        yRotation = lookJoystick.Horizontal;
+
+        Vector2 wantedVelocity = new Vector2(xRotation, yRotation) * sensitivity;
+        
+        wantedVelocity += wantedVelocity * Time.deltaTime;
+
+        transform.localEulerAngles += new Vector3 (wantedVelocity.x, wantedVelocity.y, 0);
+        }   
+    }
+
+    //MOVE: the camera left to right, forward and back
+    private void move()
+    {
+        verticleMultiplier = VerticalMultiplier();
+        horizontalMultiplier = HorizontalMultiplier();
+
+        //move forward 
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || moveJoystick.Vertical > 0 )
+        {
+            gameObject.transform.position += transform.forward * (speed * verticleMultiplier) * Time.deltaTime;
         }
 
         //move left
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || joystick.Horizontal < 0)
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || moveJoystick.Horizontal < 0)
         {
-            gameObject.transform.position += Vector3.left * speed * Time.deltaTime;
+            gameObject.transform.position += -transform.right * (speed * horizontalMultiplier) * Time.deltaTime;
         }
 
-        //move down or back w/shift key
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || joystick.Vertical < 0)
+        //move back
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || moveJoystick.Vertical < 0)
         {
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            {
-                gameObject.transform.position += Vector3.back * speed * Time.deltaTime;
-            }
-            else
-                gameObject.transform.position += Vector3.down * speed * Time.deltaTime;
+            gameObject.transform.position += -transform.forward * (speed * verticleMultiplier) * Time.deltaTime;
         }
 
         //move right
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || joystick.Horizontal > 0)
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || moveJoystick.Horizontal > 0)
         {
-            gameObject.transform.position += Vector3.right * speed * Time.deltaTime;
+            gameObject.transform.position += transform.right * (speed * horizontalMultiplier) * Time.deltaTime;
+        }
+    }
+    
+    //allows for dynamic fwd/backward joystick movement as well as keyboard movement
+    private float VerticalMultiplier ()
+    {
+        if(moveJoystick.Vertical == 0)
+        {
+            return 1;
+        }
+            
+        else
+        {
+            return Mathf.Abs(moveJoystick.Vertical);
+        }
+    }
+    
+    //allows for dynamic left/right joystick movement as well as keyboard movement
+    private float HorizontalMultiplier ()
+    {
+        if(moveJoystick.Horizontal == 0)
+        {
+            return 1;
+        }
+            
+        else
+        {
+            return Mathf.Abs(moveJoystick.Horizontal);
         }
     }
 
