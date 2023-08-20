0e4bcdbc4b5f273bc5bb132cceb042a5e1221519
Jeff MacDonald <freens1977@gmail.com>
2023-08-20 15:43:47 -0400

    Update Summary: • Reorganize code   - Added regions for Properties/Event Listeners   - General tidy up • Cleaned up Private vs. Public accessors • Put a range on Speed/Sensitivity properties

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    //CLASS PROPERTIES
+    #region Private Class Properties
+
     //New Input System
     private PlayerInputActions playerInputActions;  //C# class auto created/updated for the Input Action asset
     
+    //Movement Properties
     private InputAction movement; //OnEable: bind movement action from thep layerInputAction object
     private Vector2 currentMovement;
+    private bool isMoving   = false;
     
+    //Rotation Properties
     private InputAction rotation;
     private InputAction mouseRotation;
     private Vector2 currentRotationMovement;
     private Vector2 currentMouseRotationMovement;
+    private bool isRotating = false;
+        
+    //Camera
+    private new Camera camera;
     
-    public bool isMoving   = false;
-    public bool isRotating = false;
-    
-    public GameObject playerBody;
-    public new Camera camera;
-    public float sensitivity = 0.5f; 
-    public float speed = 10.0f;
+    #endregion Private Class Properties
 
+    #region Public Class Properties
+
+    public GameObject playerBody;
+
+    [Range(1, 10)]
+    public float lookSensitivity = 3.0f;
+    [Range(10, 50)]
+    public float speed = 25.0f;
+    
+    #endregion Public Class Properties
 
     //Awake is called before start
     void Awake()
@@ -55,6 +69,8 @@ public class PlayerController : MonoBehaviour
     }
 
     //Listener Setup Methods
+    #region Set Input Action Event Listeners 
+    
     private void SetActionPlayerMovementPerformed(InputAction.CallbackContext ctx)
     {
         currentMovement = ctx.ReadValue<Vector2>();
@@ -87,6 +103,7 @@ public class PlayerController : MonoBehaviour
         isRotating = false;
     }
 
+    #endregion Set Input Action Event Listeners
 
     //OnEnable is called wheen the component is toggled to true 
     void OnEnable()
@@ -99,7 +116,6 @@ public class PlayerController : MonoBehaviour
 
         mouseRotation = playerInputActions.Player.MouseRotation;
         mouseRotation.Enable();
-
 
     }
 
@@ -144,7 +160,7 @@ public class PlayerController : MonoBehaviour
 
        if(isRotating)
        {
-            Vector2 wantedVelocity = currentRotationMovement * sensitivity;
+            Vector2 wantedVelocity = currentRotationMovement * lookSensitivity;
             //rotates parent object left & right 
             gameObject.transform.localEulerAngles += new Vector3 (gameObject.transform.rotation.x, wantedVelocity.x * Time.deltaTime, 0);
 
@@ -159,18 +175,14 @@ public class PlayerController : MonoBehaviour
         if(!Mouse.current.leftButton.isPressed)
         return;
         
-        //isRotating = true;
-
-        Vector2 wantedVelocity = currentMouseRotationMovement * sensitivity;
+        Vector2 wantedVelocity = currentMouseRotationMovement * lookSensitivity;
         
         //rotates parent object left & right 
         gameObject.transform.localEulerAngles += new Vector3 (gameObject.transform.rotation.x, wantedVelocity.x * Time.deltaTime, 0);
 
         // rotates camera up and down
         camera.transform.localEulerAngles += new Vector3 (wantedVelocity.y * Time.deltaTime, 0, 0);
-
         
     }
-    
 
 }
